4b828c08d5ed1b0f740fbd42e9a4cecb9943e02e
zippo-yap <zippo-yap@gmail.com>
2016-04-29 09:29:28 +1200

    Re-write index page

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
             url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
             dataType: 'jsonp',
     // If the request is successful
-    success: function (data){
-      console.log(data.quoteText)
-      console.log(data.quoteAuthor);
-  },
-  error: function (xhr, status, error){
-      console.log('xhr: ' + xhr + '\nStatus: ' + status + '\nError: ' + error);
+        success: function (data){
+            console.log(data.quoteText)
+            console.log(data.quoteAuthor);
+        },
+        error: function (xhr, status, error){
+        console.log('xhr: ' + xhr + '\nStatus: ' + status + '\nError: ' + error);
   }
 });
 
