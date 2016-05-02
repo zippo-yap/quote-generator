7ab5f618fda3da534df2abf8e9df414f00cf914c
zippo-yap <zippo-yap@gmail.com>
2016-05-03 11:05:42 +1200

    Hide twitter button on load

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
+var quote;
+
 $(document).ready(function() {
 
+    $("#twitter").hide();
     $("#refresh").click(function() {
         $.ajax({
             url: "https://fortunecookieapi.herokuapp.com/v1/cookie",
             success: function (result) {
                 result = result[0];
-                var quote = result.fortune.message;
+                 quote = result.fortune.message;
                 var lesson = "Lesson: " + result.lesson.english + ", " + result.lesson.chinese +
                 ", " + result.lesson.pronunciation;
                 var lotto = "Lucky numbers: " + result.lotto.numbers.join(" ");
                 $("blockquote").html("<h1>" + quote + "</h1>")
                 $("blockquote").append("<p>" + lesson + "</p>")
                 $("blockquote").append("<p>" + lotto + "</p>")
-
+                $("#twitter").show();
             },
             error: function (xhr, status, error){
                 console.log("xhr: " + xhr + "\nStatus: " + status + "\nError: " + error);
@@ -22,8 +26,7 @@ $(document).ready(function() {
 
     $("#twitter").click(function(){
        //Code to tweet goes here
-
-        $("#twitter").attr("href","https://twitter.com/intent/tweet?hashtags=quotes&&text=" + quote);
+        $("#twitter").attr("href","https://twitter.com/intent/tweet?hashtags=fortunecookie&text=" + quote);
    });
 
 }); //Close document ready
