fd4d207c74c8c39561f8815bfe00487235795c7c
zippo-yap <zippo-yap@gmail.com>
2016-05-02 13:23:14 +1200

    Tidy indentation

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
- $(document).ready(function() {
+$(document).ready(function() {
 
-     $('#refresh').click(function() {
+    $("#refresh").click(function() {
         $.ajax({
             url: "https://fortunecookieapi.herokuapp.com/v1/cookie",
             success: function (result) {
                 result = result[0];
                 var quote = result.fortune.message;
-            // fortune = quote;
-            var lesson = "Lesson: " +result.lesson.english + ", " + result.lesson.chinese +
-            ", " + result.lesson.pronunciation;
-            var lotto ="Lucky numbers: " + result.lotto.numbers.join(" ");
-            $("blockquote").html("<h1>" +quote + "</h1>")
-            $("blockquote").append("<p>" +lesson + "</p>")
-            $("blockquote").append("<p>" +lotto + "</p>")
+                var lesson = "Lesson: " + result.lesson.english + ", " + result.lesson.chinese +
+                ", " + result.lesson.pronunciation;
+                var lotto = "Lucky numbers: " + result.lotto.numbers.join(" ");
+                $("blockquote").html("<h1>" + quote + "</h1>")
+                $("blockquote").append("<p>" + lesson + "</p>")
+                $("blockquote").append("<p>" + lotto + "</p>")
 
-        },
-        error: function (xhr, status, error){
-            console.log('xhr: ' + xhr + '\nStatus: ' + status + '\nError: ' + error);
-        }
+            },
+            error: function (xhr, status, error){
+                console.log("xhr: " + xhr + "\nStatus: " + status + "\nError: " + error);
+            }
         }); //End ajax
-    });// Close button
-     $('#twitter').click(function(){
-        ('#twitter').at = 'https://twitter.com/intent/tweet';
+    });// Close refresh function
+
+    $("#twitter").click(function(){
+       //Code to tweet goes here
+
+        $("#twitter").attr("href","https://twitter.com/intent/tweet?hashtags=quotes&&text=" + quote);
    });
 
-
-
-
 }); //Close document ready
-
-  
