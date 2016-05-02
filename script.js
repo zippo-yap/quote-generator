2f090360318e949f26d9f77a0c3b6c847ae041cf
zippo-yap <zippo-yap@gmail.com>
2016-05-02 13:04:03 +1200

    Append quote information to index

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
  $(document).ready(function() {
 
-     $('#quote').click(function() {
+     $('#refresh').click(function() {
         $.ajax({
             url: "https://fortunecookieapi.herokuapp.com/v1/cookie",
             success: function (result) {
                 result = result[0];
-                console.log(result);
-                console.log(result.fortune.message);
-                console.log(result.lesson.english + ", " + result.lesson.chinese +
-                  ", " + result.lesson.pronunciation);
-                console.log("Lucky numbers: " + result.lotto.numbers.join(" "));
+                var quote = result.fortune.message;
+            // fortune = quote;
+            var lesson = "Lesson: " +result.lesson.english + ", " + result.lesson.chinese +
+            ", " + result.lesson.pronunciation;
+            var lotto ="Lucky numbers: " + result.lotto.numbers.join(" ");
+            $("blockquote").html("<h1>" +quote + "</h1>")
+            $("blockquote").append("<p>" +lesson + "</p>")
+            $("blockquote").append("<p>" +lotto + "</p>")
 
-            },
-            error: function (xhr, status, error){
-                console.log('xhr: ' + xhr + '\nStatus: ' + status + '\nError: ' + error);
-            }
+        },
+        error: function (xhr, status, error){
+            console.log('xhr: ' + xhr + '\nStatus: ' + status + '\nError: ' + error);
+        }
         }); //End ajax
     });// Close button
+     $('#twitter').click(function(){
+        ('#twitter').at = 'https://twitter.com/intent/tweet';
+   });
+
+
+
+
 }); //Close document ready
+
+  
